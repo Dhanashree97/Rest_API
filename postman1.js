41ad774a64732badeb7db5cc37c679500cf0f753
Dhanashree Shetti <dhanashreeshetty08@gmail.com>
2019-07-29 18:10:40 +0530

    add files

---
+++ postman1.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var _ = require('underscore');
+var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1; 
+ 
+app.use(bodyParser.json());
+
+app.get('/', function(req,res)
+{
+res.send('Todo API root');
+});
+
+app.get('/todos', function(req,res){
+    res.json(todos);
+
+});
+// GET /todos
+app.get('/todos/:id',function(req,res){
+
+    var todoId = parseInt(req.params.id,10)  ;
+    var matchedTodo = _.findWhere(todos,{id: todoId});
+ 
+    // todos.forEach(function(todo){
+    //     if( todoId === todo.id) {
+    //         matchedTodo = todo;
+    //     }
+    // });
+
+    if(matchedTodo) {
+        res.json(matchedTodo);
+
+    }else {
+        res.status(404).send();
+    }
+   // res.send('Asking for todo with id of ' + req.params.id )
+});
+//POST /todos
+app.post('/todos', function(req,res)
+{
+var  body = req.body;
+if (!_.isBoolean(body.completed) ||!_.isString(body.description) || body.description.trim().length ===0)   {
+    return res.status(400).send();
+}
+//add id field
+body.id = todoNextId++;
+// push into the array
+todos.push(body);
+// console.log('description:' + body.description);  
+
+res.json(body);
+});
+
+
+  
+
+app.listen(PORT,function ()
+
+{
+  
+    console.log('Express listening on port' + PORT + '!');
+});
